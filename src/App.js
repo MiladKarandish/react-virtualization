a7c382bb309b9de375e432dfe661b551ea4a4cda
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2024-02-19 19:17:32 +0330

    Simple example added using react-virtuoso

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { Virtuoso } from "react-virtuoso";
+import { generateUsers } from "./data";
+import { useState, useCallback, useEffect } from "react";
+
+export default function App() {
+  const [users, setUsers] = useState(() => []);
+
+  const fetchRandomUsers = async () => {
+    const res = await fetch("https://randomuser.me/api/?results=100");
+    const data = await res.json();
+
+    setUsers((users) => [...users, ...data.results]);
+  };
+
+  const loadMore = useCallback(() => {
+    fetchRandomUsers();
+    // return setTimeout(() => {
+    //   setUsers((users) => [...users, ...generateUsers(100, users.length)]);
+    // }, 200);
+  }, [setUsers]);
+
+  useEffect(() => {
+    loadMore();
+  }, []);
+
+  return (
+    <Virtuoso
+      style={{ height: "100dvh" }}
+      data={users}
+      endReached={loadMore}
+      overscan={100}
+      itemContent={(index, user) => {
+        return (
+          <div
+            style={{
+              backgroundColor: "coral",
+              padding: "0.5rem 1rem",
+              marginBottom: "1rem",
+            }}
+          >
+            <h3>{user.name.first}</h3>
+            <p>{user.email}</p>
+            <span>{user.phone}</span>
+          </div>
+        );
+      }}
+      components={{ Footer }}
+    />
+  );
+}
+
+const Footer = () => {
+  return (
+    <div
+      style={{
+        padding: "2rem",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      Loading...
+    </div>
+  );
+};
